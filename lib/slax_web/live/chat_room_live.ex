fc9f17bb77d29c66a2e0d25750abc51c51ca1c71
LameckMeshack <orienolameck327@gmail.com>
2024-09-13 18:03:13 +0300

    show room info in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,11 +1,26 @@
 defmodule SlaxWeb.ChatRoomLive do
+  alias Slax.Repo
+  alias Slax.Chat.Room
+
   use SlaxWeb, :live_view
 
   def render(assigns) do
     ~H"""
-    <div>
-      <div>Welcome to the chat!</div>
+    <div class="flex flex-col flex-grow shadow-lg">
+      <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-sm font-bold leading-none">
+            #<%= assigns.room.name %>
+          </h1>
+          <div class="text-xs leading-none h-3.5"><%= assigns.room.topic %></div>
+        </div>
+      </div>
     </div>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    room = Room |> Repo.all() |> List.first()
+    {:ok, assign(socket, :room, room)}
   end
 end
